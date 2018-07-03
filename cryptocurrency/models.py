936c49e3c4e8e8b30c2112f593e5fe596b6d3d3b
carlos gomez <gomezc958@hotmail.com>
2018-07-03 00:35:10 +0000

    win

--- cryptocurrency/models.py
+++ cryptocurrency/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from rest_framework import serializers
 
-# Create your models here.
+class Notification(models.Modal):
+    id = models.AutoField(primary_key=True)
+    currency = models.CharField(max_length = 7)
+    
+    
